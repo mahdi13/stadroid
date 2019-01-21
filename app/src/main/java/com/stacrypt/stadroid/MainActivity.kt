b145c4b8d3b3972d12fd52387c658f1afa5f6715
Mahdi Aali <mahdi13.1373@gmail.com>
2019-01-21 22:02:03 +0300

    icon has been changes

--- app/src/main/java/com/stacrypt/stadroid/MainActivity.kt
+++ app/src/main/java/com/stacrypt/stadroid/MainActivity.kt
@@ -1,34 +1,28 @@
 package com.stacrypt.stadroid
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_wallet)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_market)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_profile)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+        navigation.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.navigation_home -> {
+                    message.setText(R.string.title_wallet)
+                }
+                R.id.navigation_dashboard -> {
+                    message.setText(R.string.title_market)
+                }
+                R.id.navigation_notifications -> {
+                    message.setText(R.string.title_profile)
+                }
+            }
+            true
+        }
     }
 }
