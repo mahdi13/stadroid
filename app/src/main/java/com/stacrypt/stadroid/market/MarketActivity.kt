07c1cae0a00e38a9b89eecd1658a64a15810b7c6
Mahdi Aali <mahdi13.1373@gmail.com>
2019-01-29 21:58:13 +0300

    custom dataset added

---
+++ app/src/main/java/com/stacrypt/stadroid/market/MarketActivity.kt
@@ -0,0 +1,16 @@
+package com.stacrypt.stadroid.market
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.stacrypt.stadroid.R
+
+import kotlinx.android.synthetic.main.activity_market.*
+
+class MarketActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_market)
+    }
+
+}
