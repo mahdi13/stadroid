d965232483f865f1a5df5076a2dff71c99eac9fb
Mahdi Aali <mahdi13.1373@gmail.com>
2019-02-07 00:14:22 +0300

    Add market Mine fragment and recycle adapter

--- app/src/main/java/com/stacrypt/stadroid/market/MarketMineFragment.kt
+++ app/src/main/java/com/stacrypt/stadroid/market/MarketMineFragment.kt
@@ -1,100 +1,45 @@
 package com.stacrypt.stadroid.market
 
-import android.content.Context
 import android.os.Bundle
-import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.stacrypt.stadroid.R
+import com.stacrypt.stadroid.data.Book
+import com.stacrypt.stadroid.data.Mine
 
-import com.stacrypt.stadroid.market.dummy.DummyContent
-import com.stacrypt.stadroid.market.dummy.DummyContent.DummyItem
 
-/**
- * A fragment representing a list of Items.
- * Activities containing this fragment MUST implement the
- * [MarketMineFragment.OnListFragmentInteractionListener] interface.
- */
 class MarketMineFragment : Fragment() {
 
-    // TODO: Customize parameters
-    private var columnCount = 1
-
-    private var listener: OnListFragmentInteractionListener? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        arguments?.let {
-            columnCount = it.getInt(ARG_COLUMN_COUNT)
-        }
-    }
+    private lateinit var viewModel: MarketViewModel
+    private lateinit var adapter: MarketMineRecyclerViewAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_market_mine_list, container, false)
+        val recyclerView = inflater.inflate(R.layout.fragment_market_mine_list, container, false) as RecyclerView
 
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
-                }
-                adapter = MarketMineRecyclerViewAdapter(DummyContent.ITEMS, listener)
-            }
-        }
-        return view
+        adapter = MarketMineRecyclerViewAdapter(ArrayList())
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = adapter
+
+        return recyclerView
     }
 
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-        if (context is OnListFragmentInteractionListener) {
-            listener = context
-        } else {
-//            throw RuntimeException(context.toString() + " must implement OnListFragmentInteractionListener")
-        }
-    }
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(MarketViewModel::class.java)
+        viewModel.mine.observe(this,
+            Observer<List<Mine>> { mines ->
+                adapter.items = mines
+                adapter.notifyDataSetChanged()
+            })
 
-    override fun onDetach() {
-        super.onDetach()
-        listener = null
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     *
-     *
-     * See the Android Training lesson
-     * [Communicating with Other Fragments](http://developer.android.com/training/basics/fragments/communicating.html)
-     * for more information.
-     */
-    interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        fun onListFragmentInteraction(item: DummyItem?)
-    }
-
-    companion object {
-
-        // TODO: Customize parameter argument names
-        const val ARG_COLUMN_COUNT = "column-count"
-
-        // TODO: Customize parameter initialization
-        @JvmStatic
-        fun newInstance(columnCount: Int) =
-            MarketMineFragment().apply {
-                arguments = Bundle().apply {
-                    putInt(ARG_COLUMN_COUNT, columnCount)
-                }
-            }
     }
 }
--- app/src/main/java/com/stacrypt/stadroid/market/MarketMineRecyclerViewAdapter.kt
+++ app/src/main/java/com/stacrypt/stadroid/market/MarketMineRecyclerViewAdapter.kt
@@ -6,33 +6,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import com.stacrypt.stadroid.R
+import com.stacrypt.stadroid.data.Mine
 
-
-import com.stacrypt.stadroid.market.MarketMineFragment.OnListFragmentInteractionListener
-import com.stacrypt.stadroid.market.dummy.DummyContent.DummyItem
 
 import kotlinx.android.synthetic.main.fragment_market_mine.view.*
 
-/**
- * [RecyclerView.Adapter] that can display a [DummyItem] and makes a call to the
- * specified [OnListFragmentInteractionListener].
- * TODO: Replace the implementation with code for your data type.
- */
 class MarketMineRecyclerViewAdapter(
-    private val mValues: List<DummyItem>,
-    private val mListener: OnListFragmentInteractionListener?
+    var items: List<Mine>
 ) : RecyclerView.Adapter<MarketMineRecyclerViewAdapter.ViewHolder>() {
-
-    private val mOnClickListener: View.OnClickListener
-
-    init {
-        mOnClickListener = View.OnClickListener { v ->
-            val item = v.tag as DummyItem
-            // Notify the active callbacks interface (the activity, if the fragment is attached to
-            // one) that an item has been selected.
-            mListener?.onListFragmentInteraction(item)
-        }
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -41,7 +22,7 @@ class MarketMineRecyclerViewAdapter(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = mValues[position]
+        val item = items[position]
         holder.mIdView.text = item.id
         holder.mContentView.text = item.content
 
@@ -51,14 +32,11 @@ class MarketMineRecyclerViewAdapter(
         }
     }
 
-    override fun getItemCount(): Int = mValues.size
+    override fun getItemCount(): Int = items.size
 
     inner class ViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
         val mIdView: TextView = mView.item_number
         val mContentView: TextView = mView.content
 
-        override fun toString(): String {
-            return super.toString() + " '" + mContentView.text + "'"
-        }
     }
 }
