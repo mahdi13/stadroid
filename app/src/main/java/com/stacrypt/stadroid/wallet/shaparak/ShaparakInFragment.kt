be65c331f3c58c89b850b3d0111764c3975be74a
Mahdi Aali <mahdi13.1373@gmail.com>
2019-03-15 22:30:10 +0300

    Add shaparak-in and shaparak-out fragments

---
+++ app/src/main/java/com/stacrypt/stadroid/wallet/shaparak/ShaparakInFragment.kt
@@ -0,0 +1,22 @@
+package com.stacrypt.stadroid.wallet.shaparak
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+import com.stacrypt.stadroid.R
+
+class ShaparakInFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_shaparak_in, container, false)
+    }
+
+
+}
