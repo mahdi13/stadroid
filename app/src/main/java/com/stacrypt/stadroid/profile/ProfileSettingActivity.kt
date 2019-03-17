10accc84e5e61c0ff83ec42babe2ef4547182703
Mahdi Aali <mahdi13.1373@gmail.com>
2019-03-17 22:12:01 +0300

    Add DoVerifyEmail

--- app/src/main/java/com/stacrypt/stadroid/profile/ProfileSettingActivity.kt
+++ app/src/main/java/com/stacrypt/stadroid/profile/ProfileSettingActivity.kt
@@ -19,35 +19,40 @@ class ProfileSettingActivity : AppCompatActivity() {
     }
 
     companion object {
-        val ARG_TARGET = "target"
+        const val ARG_TARGET = "target"
+        const val ARG_ACTION = "action"
+        const val ARG_TOKEN = "token"
 
-        val TARGET_VERIFICATION_PROCESS = "verification_process"
-        val TARGET_VERIFICATION_EMAIL = "email_verification"
-        val TARGET_FIXED_PHONE_VERIFICATION = "mobile_phone_verification"
-        val TARGET_MOBILE_PHONE_VERIFICATION = "fixed_phone_verification"
-        val TARGET_EVIDENCE_VERIFICATION = "evidence_verification"
+        const val ACTION_VERIFY = "verify"
 
-        val TARGET_BANK_CARDS = "bank_cards"
-        val TARGET_ADD_BANK_CARD = "add_bank_card"
-        val TARGET_BANK_ACCOUNTS = "bank_accounts"
-        val TARGET_ADD_BANK_ACCOUNT = "bank_accounts"
+        const val TARGET_VERIFICATION_PROCESS = "verification_process"
+        const val TARGET_VERIFICATION_EMAIL = "email_verification"
+        const val TARGET_DO_VERIFY_EMAIL = "do_verify_email"
+        const val TARGET_FIXED_PHONE_VERIFICATION = "mobile_phone_verification"
+        const val TARGET_MOBILE_PHONE_VERIFICATION = "fixed_phone_verification"
+        const val TARGET_EVIDENCE_VERIFICATION = "evidence_verification"
 
-        val TARGET_APPLICATION_PIN = "application_pin"
-        val TARGET_WIPE_CACHE = "whip_cache"
+        const val TARGET_BANK_CARDS = "bank_cards"
+        const val TARGET_ADD_BANK_CARD = "add_bank_card"
+        const val TARGET_BANK_ACCOUNTS = "bank_accounts"
+        const val TARGET_ADD_BANK_ACCOUNT = "bank_accounts"
 
-        val TARGET_WITHDRAWAL_PIN = "withdrawal_pin"
-        val TARGET_WITHDRAW_LIMITAITONS = "withdrawal_limitations"
+        const val TARGET_APPLICATION_PIN = "application_pin"
+        const val TARGET_WIPE_CACHE = "whip_cache"
 
-        val TARGET_SESSIONS = "bank_accounts"
-        val TARGET_SECOND_FACTOR = "second_factor"
-        val TARGET_API_KEYS = "second_factor"
-        val TARGET_IP_WHITELIST = "second_factor"
-        val TARGET_SECURITY_LOGS = "second_factor"
-        val TARGET_CHANEG_PASSWORD = "second_factor"
+        const val TARGET_WITHDRAWAL_PIN = "withdrawal_pin"
+        const val TARGET_WITHDRAW_LIMITAITONS = "withdrawal_limitations"
 
-        val TARGET_TICKETS = "tickets"
-        val TARGET_TICKET_THREAD = "ticket_thread"
-        val TARGET_NEW_TICKETS = "new_ticker"
+        const val TARGET_SESSIONS = "bank_accounts"
+        const val TARGET_SECOND_FACTOR = "second_factor"
+        const val TARGET_API_KEYS = "second_factor"
+        const val TARGET_IP_WHITELIST = "second_factor"
+        const val TARGET_SECURITY_LOGS = "second_factor"
+        const val TARGET_CHANEG_PASSWORD = "second_factor"
+
+        const val TARGET_TICKETS = "tickets"
+        const val TARGET_TICKET_THREAD = "ticket_thread"
+        const val TARGET_NEW_TICKETS = "new_ticker"
     }
 
 }
