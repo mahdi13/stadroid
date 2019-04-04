a8253f44803e4a4f02d1f99fd76d8d267dfbe0c8
Mahdi Aali <mahdi13.1373@gmail.com>
2019-04-04 11:05:04 +0300

    Add evience verification viewmode;

--- app/src/main/java/io/stacrypt/stadroid/profile/verification/EvidenceFormViewModel.kt
+++ app/src/main/java/io/stacrypt/stadroid/profile/verification/EvidenceFormViewModel.kt
@@ -1,7 +1,32 @@
 package io.stacrypt.stadroid.profile.verification
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
+import androidx.lifecycle.Transformations.switchMap
 import androidx.lifecycle.ViewModel;
+import io.stacrypt.stadroid.data.Country
+import io.stacrypt.stadroid.data.UserRepository
+import java.util.*
 
 class EvidenceFormViewModel : ViewModel() {
-//    var firstName: String
+
+    val firstName = MutableLiveData<String>()
+    val lastName = MutableLiveData<String>()
+
+    val gender = MutableLiveData<String>()
+    val address = MutableLiveData<String>()
+
+    val countries = UserRepository.getCountries()
+    val selectedCountryId = MutableLiveData<Int>()
+    val states = switchMap(selectedCountryId) {
+        UserRepository.getStates(it)
+    }
+    val selectedStateId = MutableLiveData<Int>()
+    val cities = switchMap(selectedStateId) {
+        UserRepository.getCities(it)
+    }
+    val selectedCityId = MutableLiveData<Int>()
+
+    val birthday = MutableLiveData<Date>()
+
 }
