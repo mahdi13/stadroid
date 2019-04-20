6b59df4bb75cfe7bb8a1405fdff49348fd29f459
Mahdi Aali <mahdi13.1373@gmail.com>
2019-04-20 21:37:16 +0300

    Fix badge ui problem

--- app/src/main/java/io/stacrypt/stadroid/ui/badge.kt
+++ app/src/main/java/io/stacrypt/stadroid/ui/badge.kt
@@ -2,20 +2,19 @@ package io.stacrypt.stadroid.ui
 
 import com.matrixxun.starry.badgetextview.MaterialBadgeTextView
 import io.stacrypt.stadroid.R
-import org.jetbrains.anko.backgroundColor
 import org.jetbrains.anko.backgroundColorResource
 
 fun MaterialBadgeTextView.showError(message: String) {
-    text = error
+    text = message
     backgroundColorResource = R.color.real_red
 }
 
 fun MaterialBadgeTextView.showWarning(message: String) {
-    text = error
+    text = message
     backgroundColorResource = R.color.yellow
 }
 
 fun MaterialBadgeTextView.showSuccess(message: String) {
-    text = error
+    text = message
     backgroundColorResource = R.color.real_green
 }
